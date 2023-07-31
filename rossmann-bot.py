3be398423d40a070ade2691071af68b0b36c2064
natan-matos <natanclimaco@gmail.com>
2023-07-31 20:22:49 -0300

    no calculation

--- rossmann-bot.py
+++ rossmann-bot.py
@@ -94,13 +94,13 @@ def index():
 
                 # calculation
                 #d2 = d1[['store', 'prediction']].groupby('store').sum().reset_index()
-                d2 = d1.loc[1,'prediction']
+                
 
                 # send message
                 msg = 'Store Number will sell ${} in the next 6 weeks'.format(
                     #d2['store'].values[0],
                     #d2['prediction'].values[0]
-                    d2
+                    
                 )
 
                 
