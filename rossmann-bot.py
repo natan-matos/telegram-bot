52d6541660264abc5767f670de2dcf39f2a39212
natan-matos <natanclimaco@gmail.com>
2023-08-01 18:37:49 -0300

    calculation fixed

--- rossmann-bot.py
+++ rossmann-bot.py
@@ -3,8 +3,18 @@ import json
 import requests
 import os
 
-
 from flask import Flask, request, Response
+
+# get info
+#https://api.telegram.org/bot6330025305:AAGVrpNJ5P6UH6KZ6hGtYjHqDUNxpMW8mgg/getMe
+
+#local host 
+#https://001ee589f017f2.lhr.life
+#https://8610ba0698b982.lhr.life
+
+# webhook
+#https://api.telegram.org/bot6330025305:AAGVrpNJ5P6UH6KZ6hGtYjHqDUNxpMW8mgg/setWebhook?url=https://telegram-bot-6jqb.onrender.com
+# https://0b09c2ca3edf1a.lhr.life
 
 #constans
 token = '6330025305:AAGVrpNJ5P6UH6KZ6hGtYjHqDUNxpMW8mgg'
@@ -15,12 +25,9 @@ def send_message(chat_id, text):
     url = url + 'sendMessage?chat_id={}'.format( chat_id )
 
     r = requests.post(url, json={'text': text})
-    print('def send_message ok')
     print('Satus Code{}'.format(r.status_code))
 
     return None
-
-
 
 def load_data(store_id):
     # loading test data
@@ -47,83 +54,80 @@ def load_data(store_id):
     else:
         data = 'error'
 
-    print('def load_data ok')
     return data
 
 def predict(data):
     # API Call
     url = 'https://rossmann-api-t2qp.onrender.com'
-    header = {'Content-type': 'application/json' }
-    data = data
-    r = requests.post( url, data=data, headers=header )
-    print( 'Status Code {}'.format( r.status_code ) )
-    print('def predict ok')
-    
-    return data
+    header = {'Content-type': 'application/json'}
+    r = requests.post(url, data=data, headers=header)
+    print('Status Code {}'.format(r.status_code))
 
+    if r.status_code == 200:
+        try:
+            response_json = r.json()
+            df_response = pd.DataFrame(response_json)  # Convert JSON to DataFrame
+            return df_response
+        except ValueError:
+            print('Error: Invalid response from the API')
+    else:
+        print('Error: API request failed')
+    
+    return None
 
 def parse_message(message):
     chat_id = message['message']['chat']['id']
     store_id = message['message']['text']
 
     store_id = store_id.replace('/', '')
-    
+
     try:
         store_id = int(store_id)
-    
     except ValueError:
         send_message(chat_id, 'Store ID is Wrong')
         store_id = 'error'
 
-    print('def parse_message ok')
     return chat_id, store_id
 
 # API initialize
 app = Flask(__name__)
-
 @app.route('/', methods=['GET', 'POST'])
 
 def index():
     if request.method == 'POST':
-         message = request.get_json()
-         chat_id, store_id = parse_message(message)
+        message = request.get_json()
+        chat_id, store_id = parse_message(message)
 
-         if store_id != 'error':
-             #loading data
-             data = load_data(store_id)
-             
-             if data != 'error':
+        if store_id != 'error':
+            # loading data
+            data = load_data(store_id)
+
+            if data != 'error':
                 # prediction
-                d1 = predict( data )
+                df_response = predict(data)
 
-                # calculation
-                #d2 = d1[['store', 'prediction']].groupby('store').sum().reset_index()
-                
+                if df_response is not None:
+                    # calculation
+                    d2 = df_response[['store', 'prediction']].groupby('store').sum().reset_index()
 
-                # send message
-                #msg = 'Store Number {} will sell ${} in the next 6 weeks'.format(
-                    #d2['store'].values[0],
-                    #d2['prediction'].values[0]                    
-                #)
-                msg = d1.values[1]
+                    # send message
+                    msg = f'Store Number {d2["store"].values[0]} will sell ${d2["prediction"].values[0]:.2f} in the next 6 weeks'
+                    send_message(chat_id, msg)
+                    return Response('Ok', status=200)
 
-                
-                send_message( chat_id, msg )
-                return Response('Ok', status=200 )
+                else:
+                    send_message(chat_id, 'Error: Invalid response from the API')
+                    return Response('OK', status=200)
 
-             else:
-                 send_message(chat_id, 'Store Not Available')
-
-                 return Response( 'OK', status=200 )
-         else:
+            else:
+                send_message(chat_id, 'Store Not Available')
+                return Response('OK', status=200)
+        else:
             send_message(chat_id, 'Store ID is Wrong')
-            
             return Response('OK', status=200)
     else:
         return '</h1> Rossmann Telegram Bot </h1>'
-    print('def index ok')
-
-
+    
 if __name__ == '__main__':
-    port = os.environ.get('PORT', 5000)
+    port = os.environ.get('PORT', 500)
     app.run(host='0.0.0.0', port=port)
